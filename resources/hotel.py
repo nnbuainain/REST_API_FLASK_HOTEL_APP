1602b8390679ae5a884304543475efa48376342a
Nelson Buainain <nnbuainain@gmail.com>
2022-09-09 10:49:02 -0400

    create class HotelModel update put and post functions to new class

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
         {
@@ -52,7 +53,9 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        
+        novo_hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = novo_hotel_objeto.convert_to_json()
 
         hoteis.append(novo_hotel)
 
@@ -60,7 +63,9 @@ class Hotel(Resource):
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        
+        novo_hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = novo_hotel_objeto.convert_to_json()
 
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
